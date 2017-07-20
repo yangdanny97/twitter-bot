5902539d6431bfb1db7c9204f992c85bce1f02ea
yangdanny97 <dzy4@cornell.edu>
2017-07-19 22:50:45 -0700

    restructured files, added crontab and tests

--- twitterbot/bot/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Tweet(models.Model):
-	tweet_id = models.IntegerField(default=0)
-	tweet_text = models.CharField(max_length=140)
-	pub_date = models.DateTimeField('date tweeted')
