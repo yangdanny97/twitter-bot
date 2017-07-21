984936153a6a7fa4ea200e6b5663017043ecdb99
yangdanny97 <dzy4@cornell.edu>
2017-07-21 14:10:20 -0700

    recommit

---
+++ bot/tasks.py
@@ -0,0 +1,8 @@
+API_KEY = 'jtkK8JJ1rJhFuPC6ro0ibZu1c'
+API_SECRET = 'uAjfEyWIdMdUdIe8BW3IXkAvXLWD4kvftLrBkybnyujTp6DpjB'
+
+def test_schedule():
+	print('testing schedule')
+
+def bot_main():
+	pass
